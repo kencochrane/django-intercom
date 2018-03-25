7d66a1ebe373dcdf7631fa81014ba002646992c8
Emad Mokhtar <emad.m.habib@gmail.com>
2018-03-25 10:47:54 +0200

    Add tests to the package

---
+++ tests/test_settings.py
@@ -0,0 +1,20 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY = 'very-secret-key'
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'intercom'
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
