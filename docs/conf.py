01a5c8d80fb36c3da2e111d4dc9fd6caab113f6e
Santos Gallegos <santos_g@outlook.com>
2019-02-01 23:48:19 -0500

    Fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,6 @@ copyright = '2016, Ken Cochrane'
 # built documents.
 #
 sys.path.insert(0, os.pardir)
-m = __import__("intercom")
 
 version = "0.0.13"
 release = version
@@ -127,7 +126,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
